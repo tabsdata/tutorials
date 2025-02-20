ff3a3f7509379e183b9e9986a632954e240e22e2
shubhamg <shubham_gupta@berkeley.edu>
2025-02-20 14:03:03 -0600

    pre_draft

--- publisher.py
+++ publisher.py
@@ -1,13 +1,11 @@
-# Create a Publisher to import data from a CSV in your local directory
-
 import os
 import tabsdata as td
 
 td.publisher(
-    source = td.LocalFileSystem(os.path.join(os.getenv("TDX"), "persons.csv")), #Store the path to your working directory in the enviroment variable TDX.
-    tables = ["persons_t1"],
+    source = td.LocalFileSystem(os.path.join(os.getenv("TDX"), "persons.csv")), # Absolute system path to the file to be imported by the Publisher.
+    tables = ["persons_t1"], # Name of the table created in the Tabsdata collection.
 )
 
 def publish_t1(tf:td.TableFrame):
-    tf = tf.drop("name","surname","first_name","last_name","full_name","phone_number","telephone","email")
+    tf = tf.drop("name","surname","first_name","last_name","full_name","phone_number","telephone","email") # Drop columns from the input file before publishing to Tabsdata.
     return tf
--- subscriber.py
+++ subscriber.py
@@ -2,8 +2,8 @@ import os
 import tabsdata as td
 
 @td.subscriber(
-    ["persons_t1"],
-    td.LocalFileDestination(os.path.join(os.getenv("TDX"), "persons_t1_output.jsonl")),
+    ["persons_t1"], # Name of the table to be exported from Tabsdata.
+    td.LocalFileDestination(os.path.join(os.getenv("TDX"), "persons_t1_output.jsonl")), # Absolute system path to the file to be written by the Subscriber.
 )
 
 def subsribe_t1(persons_t1: td.TableFrame):
