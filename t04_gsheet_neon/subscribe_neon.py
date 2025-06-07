a29f53c007c1767cd84c1f3e6a4a9b1ec1392b35
adaydan-tabsdata <daniel@tabsdata.com>
2025-06-06 18:00:19 -0700

    Update subscribe_neon.py

--- t04_gsheet_neon/subscribe_neon.py
+++ t04_gsheet_neon/subscribe_neon.py
@@ -6,7 +6,7 @@ import tabsdata as td
     destination=td.PostgresDestination(
         uri="postgresql://ef-hard-spotlight-g63h4ojk.us-west-2.aws.neon.tech/neondb?sslmode=require",  # Postgres database URI
         destination_table=["public.event_booth_visitors"],  # Target table in Postgres
-        credentials=td.UserPasswordCredentials(td.EnvironmentSecret("neon_username"), td.EnvironmentSecret("neon_password")),
+        credentials=td.UserPasswordCredentials('db_username', 'db_password'),
         if_table_exists="replace",  # Replace table if it already exists
     ),
 )
