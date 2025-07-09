ae2e01ac90e0c023458c90dde001d749b3b6107b
adaydan-tabsdata <daniel@tabsdata.com>
2025-07-09 15:29:59 -0700

    Update sub_postgres.py

--- t05_oracle_cdc/sub_postgres.py
+++ t05_oracle_cdc/sub_postgres.py
@@ -4,7 +4,7 @@ import tabsdata as td
 @td.subscriber(
     tables=["customers", "customers_cdc"],  # Source Tabsdata table to subscribe to
     destination=td.PostgresDestination(
-        uri="postgresql://ep-soft-resonance-a67u2obd.us-west-2.aws.neon.tech/neondb?sslmode=require",  # Postgres database URI
+        uri="postgresql://ef-hard-spotlight-g63h4ojk.us-west-2.aws.neon.tech/neondb?sslmode=require",  # Postgres database URI
         destination_table=["public.customers", "public.customers_cdc"],  # Target table in Postgres
         credentials=td.UserPasswordCredentials(td.EnvironmentSecret("NEON_USERNAME"), td.EnvironmentSecret("NEON_PASSWORD")),
         if_table_exists="replace",  # Replace table if it already exists
