a031e40360801821db9eb40ffa4009c440305785
adaydan-tabsdata <daniel@tabsdata.com>
2025-10-01 16:30:33 -0700

    Update 05_s3_sub.py

--- t07_nyc_taxi_weather/functions/05_s3_sub.py
+++ t07_nyc_taxi_weather/functions/05_s3_sub.py
@@ -5,7 +5,7 @@ AWS_ACCESS_KEY = td.EnvironmentSecret("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = td.EnvironmentSecret("AWS_SECRET_KEY")
 AWS_S3_URI = td.EnvironmentSecret("AWS_S3_URI").secret_value
 AWS_REGION = td.EnvironmentSecret("AWS_REGION").secret_value
-
+AWS_GLUE_DATABASE = td.EnvironmentSecret("AWS_GLUE_DATABASE").secret_value
 
 s3_credentials=td.S3AccessKeyCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
 
@@ -21,7 +21,7 @@ s3_credentials=td.S3AccessKeyCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
             "type": "glue",
             "client.region": AWS_REGION,
         },
-        tables=["tabsdata-nyc-taxi-data.daily_metrics"],
+        tables=[f"{AWS_GLUE_DATABASE}.daily_metrics"],
         auto_create_at=[AWS_S3_URI],
         if_table_exists="replace",
         credentials = s3_credentials
