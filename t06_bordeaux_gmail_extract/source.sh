cad9545bc340474014d9a7926260ff2a08450b45
adaydan-tabsdata <daniel@tabsdata.com>
2025-10-10 08:00:15 -0700

    tutorial

---
+++ t06_bordeaux_gmail_extract/source.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright 2025. Tabs Data Inc.
+
+export TD_SERVER=?
+export TD_USER=?
+export TD_PASSWORD=?
+export TD_ROLE=?
+export TDX=?
+
+export TDX=?
+export imap_host=?
+export imap_port=?
+export imap_email_user=?
+export imap_email_password=?
+
+export databricks_host_url=?
+export databricks_token=?
+export volume=?
+export catalog=?
+export schema=?
+export warehouse=?
+
+export MYSQL_USERNAME=?
+export MYSQL_PASSWORD=?
+export MYSQL_URI=?
+
+
